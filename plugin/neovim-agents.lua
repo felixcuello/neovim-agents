7361741d87d6e66428ffa02f14479d89e201abd7
Felix <felix.cuello@gmail.com>
2026-02-03 10:31:14 -0300

    fix: Renamed neovim-cursor to neovim-agents

--- plugin/neovim-agents.lua
+++ plugin/neovim-agents.lua
@@ -2,10 +2,10 @@
 -- This file is automatically loaded by Neovim
 
 -- Prevent loading the plugin twice
-if vim.g.loaded_neovim_cursor then
+if vim.g.loaded_neovim_agents then
   return
 end
-vim.g.loaded_neovim_cursor = true
+vim.g.loaded_neovim_agents = true
 
 -- Setup the plugin with default configuration
 -- Users can override this by calling require('neovim-agents').setup() in their config
